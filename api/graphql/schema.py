d33ac21105ffdd7771e31de4ee82b6fead9d2dc8
14D00944 <14D00944@skvdiweb.com>
2020-04-10 18:03:56 +0900

    setting schema resolve

--- api/graphql/schema.py
+++ api/graphql/schema.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import graphene
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
@@ -8,36 +9,33 @@ from api.graphql.models import GameRanking as GameRankingModel
 
 
 class Department(MongoengineObjectType):
-
-    class Meta:
-        model = DepartmentModel
-        interfaces = (Node,)
-
+  class Meta:
+    model = DepartmentModel
+    interfaces = (Node,)
 
 class Role(MongoengineObjectType):
-
-    class Meta:
-        model = RoleModel
-        interfaces = (Node,)
-
+  class Meta:
+    model = RoleModel
+    interfaces = (Node,)
 
 class Employee(MongoengineObjectType):
-
-    class Meta:
-        model = EmployeeModel
-        interfaces = (Node,)
+  class Meta:
+    model = EmployeeModel
+    interfaces = (Node,)
 
 class GameRanking(MongoengineObjectType):
+  class Meta:
+    model = GameRankingModel
+    interfaces = (Node,)
 
-    class Meta:
-        model = GameRankingModel
-        interfaces = (Node,)
+  def resolve_reg_dttm(parent, info):
+    return datetime.strptime(parent.reg_dttm, "%Y%m%d%H%M%S").strftime("%Y-%m-%d %H:%M:%S")
 
 class Query(graphene.ObjectType):
-    node = Node.Field()
-    all_employees = MongoengineConnectionField(Employee)
-    all_role = MongoengineConnectionField(Role)
-    role = graphene.Field(Role)
-    ranking = MongoengineConnectionField(GameRanking)
+  node = Node.Field()
+  all_employees = MongoengineConnectionField(Employee)
+  all_role = MongoengineConnectionField(Role)
+  role = graphene.Field(Role)
+  ranking = MongoengineConnectionField(GameRanking)
 
 schema = graphene.Schema(query=Query, types=[Department, Employee, Role, GameRanking])
